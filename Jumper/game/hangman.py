0d0943d0ab743921c60e713976ffe965d32cbe17
RedIts <redito.dean04@gmail.com>
2022-02-07 16:47:38 +0800

    Changes to hangman:     method to display jumper.
    Subject to changes:
        false_count - placeholder for boolean false count.
                    - responsibility is to get false boolean counts and use
                        as value for displaying elements in self._parachute

--- Jumper/game/hangman.py
+++ Jumper/game/hangman.py
@@ -1,13 +1,37 @@
 class Hangman:
+  """
+    The responsibility of Hangman is to display the parachute and delete the chute from top depending on the count of false boolean returns
+
+    Attributes:
+        _parachute (List[str]): List to make an image of parachute. 
+  """
   def __init__(self):
-      
-      """
-     ___
-    /   \
-    \   /
-     \ /
-      O
-     /|\
-     / \ 
-      
-      """
+    """
+    Constructs parachute
+
+    Args:
+      self (Hangman): An instance of Hangman
+    """
+    self._parachute = [" ___ ",
+                       "/___\ ",
+                       "\   / ",
+                       " \ / ",
+                       "  O ",
+                       " /|\ ",
+                       " / \ "]
+  
+  def update_chute(self, false_count):
+    """
+    Updates the parachute on the count of false boolean return.
+    """
+    if false_count == 0:
+      for x in range(len(self._parachute)):
+        print(self._parachute[x])
+
+    elif false_count == 4:
+      print("  x \n"
+            " /|\ \n"
+            " / \ \n")
+    else:
+      for x in range(false_count, len(self._parachute)):
+        print(self._parachute[x])
